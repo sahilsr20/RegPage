de7f765dd73359bc9e1e56d376727b7dd53c5f89
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-14 09:03:03 +0530

    reg updated

--- src/Components/Home/UpCommingRegisterDiv.js
+++ src/Components/Home/UpCommingRegisterDiv.js
@@ -6,7 +6,16 @@ export default function UpCommingRegisterDiv() {
     <div className={RegCss.RegmDiv}>
       <div class={RegCss.flexDiv}>
         <div>date</div>
-        <div>Lorem, ipsum.</div>
+        <div>
+          <h1>Lorem, ipsum.</h1>
+          <p>
+            Ridiculus cras mi vivamus placera ridiculus cras mi vivamus placerat
+            aliquam et id. Ut scelerisque in non integert aliquam et id. Ut
+            scelerisque in non integer Ut scelerisque in non integert aliquam et
+            id. Ut scelerisque in non integerut scelerisque in non integert
+            aliquam et id. Ut scelerisque in non integer.
+          </p>
+        </div>
       </div>
     </div>
   );
