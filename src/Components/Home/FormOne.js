42a64f5d0130782cea47aff733405357dd5bf0c6
achyut vardhan <achyutvardhan234@gmail.com>
2022-01-14 20:30:14 +0530

    Create FormOne.js

---
+++ src/Components/Home/FormOne.js
@@ -0,0 +1,23 @@
+import React from "react";
+import BasicTextInput from "../components/BasicITextInput";
+import RadioInputs from "../components/RadioInputs";
+
+const FormOne = () => {
+    return (<
+        div >
+        <
+            BasicTextInput name="FirstName"
+            label="Basic Details" />
+        <
+            BasicTextInput name="LastName" />
+        <
+            BasicTextInput name="Mobile Number"
+            label="Mobile Number" />
+        <
+            RadioInputs question="Pronoun"
+            radioList={["He/Him", "She/Her", "They/Them", "jkafhsjak"]
+            }/> </div>
+        );
+};
+
+        export default FormOne;
