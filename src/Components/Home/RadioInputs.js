d9bebc7cb6fb673c0ba27a3c87ae94be79fdd23a
AnubhavRawat007 <75930933+AnubhavRawat007@users.noreply.github.com>
2022-01-14 17:39:37 +0530

    input components

---
+++ src/Components/Home/RadioInputs.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const RadioInput = (props) => {
+  return (
+    <div>
+      <input type="radio" name={props.name} value={props.name}></input>
+      <label htmlFor={props.name}>{props.name}</label>
+    </div>
+  );
+};
+
+const RadioInputs = (props) => {
+  // question and radiolist
+  console.log(props.radioList);
+  return (
+    <div>
+      <h2>{props.question}</h2>
+      {props.radioList.map((value) => {
+        return <RadioInput key={value} name={value} />;
+      })}
+    </div>
+  );
+};
+
+export default RadioInputs;
