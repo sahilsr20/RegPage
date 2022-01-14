b118e747a1e2b71ac113d1f4010dec58c6d18672
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-14 09:15:09 +0530

    reg divs styled

--- src/Components/Home/UpCommingRegisterDiv.js
+++ src/Components/Home/UpCommingRegisterDiv.js
@@ -1,14 +1,20 @@
 import React from "react";
 import RegCss from "./CSS/UpCommingRegisterDiv.module.css";
 
+import img0 from "./../../ReqImg/hammer.svg";
+
 export default function UpCommingRegisterDiv() {
   return (
     <div className={RegCss.RegmDiv}>
       <div class={RegCss.flexDiv}>
-        <div>date</div>
+        <div className={RegCss.DateDiv}>
+          <p>29</p>
+          <p className={RegCss.monthName}>January</p>
+          <img src={img0} alt="HammerImg" className={RegCss.hammerImg} />
+        </div>
         <div>
-          <h1>Lorem, ipsum.</h1>
-          <p>
+          <h1 className={RegCss.h1Title}>Lorem, ipsum.</h1>
+          <p className={RegCss.des}>
             Ridiculus cras mi vivamus placera ridiculus cras mi vivamus placerat
             aliquam et id. Ut scelerisque in non integert aliquam et id. Ut
             scelerisque in non integer Ut scelerisque in non integert aliquam et
