d60959e3d12f9ed015cc8cc195bf846a69bdb358
achyut vardhan <achyutvardhan234@gmail.com>
2022-01-15 18:03:29 +0530

    Update Frame31.js

--- src/Components/Home/Frame31.js
+++ src/Components/Home/Frame31.js
@@ -1,12 +1,13 @@
 import React from "react";
-import BasicTextInput from "./BasicITextInput";
 import Option from "./Option";
+
 const Frame31 = () => {
-        return ( <div>
-            <BasicTextInput label = "Playing with a team?" />
-            <Option option = "Going Solo" />
-            < Option option = "Imma team player" />
-            <Option option = "Back" />
-            </div> );
-        };
-        export default Frame31;
+  return (
+    <div>
+      <p>Playing with a team?</p>
+      <Option option="Going Solo" />
+      <Option option="Imma team player" />
+    </div>
+  );
+};
+export default Frame31;
