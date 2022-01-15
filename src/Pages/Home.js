5c44d6fb147b8693474fb5edf1909c4f0d523e47
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-15 16:21:29 +0530

    home updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Nav from "./../Components/Home/Nav";
 import Events from "../Components/Home/Events";
+import Sliders from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import FormOne from "./../Components/Form/FormOne";
+import "./CSS/Test.css";
 
 export default function Home() {
+  var settings = {
+    dots: false,
+    infinite: false,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+  };
   return (
-    <div>
+    <div className="HnDiv">
       <Nav />
       <Events
         eventName="Upcoming Events"
@@ -21,6 +35,15 @@ export default function Home() {
         btnName0="Comming Soon 🚀"
         btnName1="Comming Soon 🚀"
       />
+      <div className="textDiv">
+        <Sliders {...settings} className="slide">
+          <div className="home">
+            <FormOne />
+          </div>
+          <div className="home">News</div>
+          <div className="home">Contact</div>
+        </Sliders>
+      </div>
     </div>
   );
 }
