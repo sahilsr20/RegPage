c2ca836a93784ddc29d9bf684858f858c417cd9f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-18 00:21:58 +0530

    nav bar res. added

--- src/Components/Home/Nav.js
+++ src/Components/Home/Nav.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import HCss from "./CSS/Nav.module.css";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 import NavEventsDiv from "./NavEventsDiv";
 import { useDispatch } from "react-redux";
 // import Darkmode from "darkmode-js";
@@ -14,32 +14,53 @@ import img2 from "./../../ReqImg/LightDarkIcon.svg";
 export default function Nav(props) {
   const dispatch = useDispatch();
   // const [iniImg, setImg] = useState(img0);
-  const isLightTheme = useSelector((state) => state.isLightTheme)
+  const isLightTheme = useSelector((state) => state.isLightTheme);
 
   const hamTog = async function (e) {
     document.getElementById("mDiv").style.display = "block";
-    document.getElementById("hideImg").style.opacity = "0";
   };
   const toggleLightMode = () => {
     dispatch({ type: "toggleTheme" });
   };
 
   return (
-    <div className={HCss.HmDiv} id={isLightTheme === false? "darkModeNav" : ""}>
+    <div
+      className={HCss.HmDiv}
+      id={isLightTheme === false ? "darkModeNav" : ""}
+    >
       <NavMobo />
       <div className={HCss.hamburgwerDiv} onClick={hamTog}>
-        <div className={HCss.hamLine1} id={isLightTheme === false? "darkHamLine" : ""}></div>
-        <div className={HCss.hamLine2} id={isLightTheme === false? "darkHamLine" : ""}></div>
-        <div className={HCss.hamLine3} id={isLightTheme === false? "darkHamLine" : ""}></div>
+        <div
+          className={HCss.hamLine1}
+          id={isLightTheme === false ? "darkHamLine" : ""}
+        ></div>
+        <div
+          className={HCss.hamLine2}
+          id={isLightTheme === false ? "darkHamLine" : ""}
+        ></div>
+        <div
+          className={HCss.hamLine3}
+          id={isLightTheme === false ? "darkHamLine" : ""}
+        ></div>
       </div>
       <div className={HCss.logoDiv}>
         <img src={img1} alt="FedLogo" />
-        <p className={HCss.fedP} id={isLightTheme === false? "darkModeTitle" : ""}>FED</p>
+        <p
+          className={HCss.fedP}
+          id={isLightTheme === false ? "darkModeTitle" : ""}
+        >
+          FED
+        </p>
       </div>
       <div className={HCss.rightDiv}>
         <div className={HCss.logoDivMobo}>
           <img src={img1} alt="FedLogo" />
-          <p className={HCss.fedP} id={isLightTheme === false? "darkModeTitle" : ""}>FED</p>
+          <p
+            className={HCss.fedP}
+            id={isLightTheme === false ? "darkModeTitle" : ""}
+          >
+            FED
+          </p>
         </div>
         <div className={HCss.tabs}>
           <NavEventsDiv pTag="Events" New="New!" />
@@ -53,7 +74,7 @@ export default function Nav(props) {
           <img
             src={img0}
             alt="LightDarkImg"
-            className={HCss.LightDarkImg} 
+            className={HCss.LightDarkImg}
             onClick={toggleLightMode}
           />
         </div>
--- src/Components/Home/NavMobo.js
+++ src/Components/Home/NavMobo.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 import NavEventsDiv from "./NavEventsDiv";
-import NavEventsMobo from './NavEventsMobo';
+import NavEventsMobo from "./NavEventsMobo";
 import "./CSS/MoboPop.css";
 
 export default function NavMobo() {
-    const btnClose = async function (e) {
-        document.getElementById('mDiv').style.display = "none";
-        document.getElementById('hideImg').style.opacity = "1";
-    }
-    return (
-        <div id='mDiv'>
-            <div className="cont">
-                <div className="closeBtnDiv">
-                    <button onClick={btnClose} className="closeBtnMobo">X</button>
-                </div>
-                <div>
-                    <NavEventsMobo pTag="Events" new="New!" />
-                    <p className="EventP">Podcasts</p>
-                    <p className="EventP">Blogs</p>
-                    <p className="EventP">Our Team</p>
-                    <p className="EventP">Join us</p>
-                    <NavEventsMobo pTag="Design System" new="New!" />
-                </div>
-            </div>
+  const btnClose = async function (e) {
+    document.getElementById("mDiv").style.display = "none";
+  };
+  return (
+    <div id="mDiv">
+      <div className="cont">
+        <div className="closeBtnDiv">
+          <button onClick={btnClose} className="closeBtnMobo">
+            X
+          </button>
         </div>
-    )
+        <div>
+          <NavEventsMobo pTag="Events" new="New!" />
+          <p className="EventP">Podcasts</p>
+          <p className="EventP">Blogs</p>
+          <p className="EventP">Our Team</p>
+          <p className="EventP">Join us</p>
+          <NavEventsMobo pTag="Design System" new="New!" />
+        </div>
+      </div>
+    </div>
+  );
 }
